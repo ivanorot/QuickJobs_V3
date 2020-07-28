e362c80615a2a56de95c2d422597c2ab36c2d949
Ivan Orozco <ivan.orot93@gmail.com>
2020-07-27 18:41:57 -0700

    7/27/2020 CameraX works -Working on Resolution and crop on ImageCapture(passing code from NavHost branch to Navigation_Backstack_fix)

--- app/src/main/java/com/example/quickjobs/view/jobs/CameraActivity.java
+++ app/src/main/java/com/example/quickjobs/view/jobs/CameraActivity.java
@@ -1,12 +1,12 @@
 package com.example.quickjobs.view.jobs;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -37,97 +37,126 @@ public class CameraActivity extends AppCompatActivity {
     private Preview cameraPreview;
     private ImageCapture imageCapture;
     private Camera camera;
-    private Button image_capture_Button;
+    private ImageButton image_capture_Button;
     private Bitmap images[] = new Bitmap[10];
+    private ImageView imageview;
 
     private static int REQUESTED_CODE_PERMISSIONS = 10;
     private static String[] REQUIRED_PERSMISSIONS = {Manifest.permission.CAMERA};
-    private ExecutorService initCameraExecutor;
+    private ExecutorService cameraExecutor;
     private PreviewView previewView;
     private ProcessCameraProvider cameraProvider;
+    private CameraSelector cameraSelector;
+
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        cameraExecutor = Executors.newSingleThreadExecutor();
+        //CameraXConfig cameraX = new CameraXConfig().;
+
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        image_capture_Button = (ImageButton) findViewById(R.id.cameraActivity_capture_imageButton);
+        imageview = (ImageView) findViewById(R.id.cameraActivity_imageView);
+
+
+        previewView = (PreviewView) findViewById(R.id.cameraActivity_viewFinder);
+
 
         cameraPermission();
-        previewView.findViewById(R.id.cameraActivity_FrameLayout);
 
-        image_capture_Button.findViewById(R.id.cameraActivity_capture_imageButton);
+
         setButtonClickListener();
 
-        ExecutorService cameraExecutor = Executors.newSingleThreadExecutor();
 
 
     }
 
-    private void cameraPermission(){
-        if(allPermissionGranted()){
+    private void cameraPermission() {
+        if (allPermissionGranted()) {
             startCamera();
-        }
-        else{
+            Toast.makeText(this, "Permissions Granted.", Toast.LENGTH_SHORT).show();
+        } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERSMISSIONS, REQUESTED_CODE_PERMISSIONS);
+            Toast.makeText(this, "Not all permission granted", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void setButtonClickListener(){
-        if(image_capture_Button!=null){
-            image_capture_Button.setOnClickListener(takePhoto());
-        }
-    }
+    private void setButtonClickListener() {
 
-    private View.OnClickListener takePhoto(){
-
-        imageCapture.takePicture(ContextCompat.getMainExecutor(this), new ImageCapture.OnImageCapturedCallback() {
+        image_capture_Button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCaptureSuccess(@NonNull ImageProxy image) {
-                super.onCaptureSuccess(image);
+            public void onClick(View v) {
+                //Toast.makeText(v.getContext(), "Button works.", Toast.LENGTH_SHORT).show();
+                imageCapture = new ImageCapture.Builder().build();
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle((LifecycleOwner)v.getContext(), cameraSelector, imageCapture, cameraPreview);
+                imageCapture.takePicture(cameraExecutor, new ImageCapture.OnImageCapturedCallback() {
+                    @Override
+                    public void onCaptureSuccess(@NonNull ImageProxy image) {
+                        super.onCaptureSuccess(image);
+                        Toast.makeText(v.getContext(), "Image Capture papaw.", Toast.LENGTH_SHORT).show();
+                        Bitmap bitmap = convertImageProxytoBitmap(image);
+                        saveImageIntoArray(bitmap);
+                        displayImages();
+                        image.close();
+                    }
 
-                Bitmap bitmap = convertImageProxytoBitmap(image);
-                saveImageIntoArray(bitmap);
-
-
+                    ;
+                });
             }
-        });
 
-        return null;
+            ;
+        });
     }
 
-    private void saveImageIntoArray(Bitmap tempBitmap){
+    private void displayImages(){
+        Toast.makeText(this, "Image Capture papaw.", Toast.LENGTH_SHORT).show();
+
+        imageview.setVisibility(View.VISIBLE);
+        imageview.setImageBitmap(images[0]);
+    }
+
+
+    private void saveImageIntoArray(Bitmap tempBitmap) {
         END:
-        for(int i = 0; i<10; i++){
-           if(images[i]==null) {
-               images[i] = tempBitmap;
-               break END;
-           }
+        for (int i = 0; i < 10; i++) {
+            if (images[i] == null) {
+                images[i] = tempBitmap;
+                break END;
+            }
         }
     }
 
-    private Bitmap convertImageProxytoBitmap(ImageProxy image){
+    private Bitmap convertImageProxytoBitmap(ImageProxy image) {
         ByteBuffer buffer = image.getPlanes()[0].getBuffer();
         buffer.rewind();
-        byte [] bytes = new byte[buffer.capacity()];
+        byte[] bytes = new byte[buffer.capacity()];
         buffer.get(bytes);
         byte[] tempBytes = bytes.clone();
 
         return BitmapFactory.decodeByteArray(tempBytes, 0, tempBytes.length);
     }
 
-    private void startCamera(){
-        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+    private void startCamera() {
 
-        cameraProviderFuture.addListener(()->{
+
+        cameraProviderFuture.addListener(() -> {
             try {
+
                 cameraProvider = cameraProviderFuture.get();
                 bindPreview();
 
-            }
-            catch(ExecutionException|InterruptedException e){
+            } catch (ExecutionException | InterruptedException e) {
+
             }
 
-        }, ContextCompat.getMainExecutor(this));
+        }, ActivityCompat.getMainExecutor(this));
 
 
     }
@@ -135,36 +164,46 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUESTED_CODE_PERMISSIONS){
-            if(allPermissionGranted()){
+        if (requestCode == REQUESTED_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
                 startCamera();
-            }else{
+            } else {
                 Toast.makeText(this, "Permission not granted by user.", Toast.LENGTH_SHORT).show();
                 finish();
             }
         }
     }
 
-    private boolean allPermissionGranted(){
+    private boolean allPermissionGranted() {
         int permissions_count = REQUIRED_PERSMISSIONS.length;
-        for (int i = 0; i<permissions_count; i++)
-            if(ContextCompat.checkSelfPermission(getBaseContext(), REQUIRED_PERSMISSIONS[i])!=PackageManager.PERMISSION_GRANTED){
+        for (int i = 0; i < permissions_count; i++) {
+            if (ContextCompat.checkSelfPermission(getBaseContext(), REQUIRED_PERSMISSIONS[i]) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permissions failed.", Toast.LENGTH_SHORT).show();
+
                 return false;
             }
+        }
+
         return true;
     }
 
-    private void bindPreview(){
+    private void bindPreview() {
         cameraPreview = new Preview.Builder().build();
-        CameraSelector cameraSelector = new CameraSelector.Builder()
+        cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
-        cameraPreview.setSurfaceProvider(previewView.createSurfaceProvider());
-        camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, cameraPreview);
+        try {
+            Toast.makeText(this, "Binding.", Toast.LENGTH_SHORT).show();
+            cameraProvider.unbindAll();
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, cameraPreview);
+            previewView.setPreferredImplementationMode(PreviewView.ImplementationMode.TEXTURE_VIEW);
+            cameraPreview.setSurfaceProvider(previewView.createSurfaceProvider());
 
+
+
+        } catch (Exception e) {
+
+        }
     }
-
-
-
-
+}
 }
