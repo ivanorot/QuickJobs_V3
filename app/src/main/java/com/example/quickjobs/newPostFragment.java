63becb45cddf7c774e5467cd5f19aa733f49f547
Ivan Orozco <ivan.orot93@gmail.com>
2020-08-10 18:29:50 -0700

    8/10/2020 quick save

--- app/src/main/java/com/example/quickjobs/newPostFragment.java
+++ app/src/main/java/com/example/quickjobs/newPostFragment.java
@@ -1,6 +1,8 @@
 package com.example.quickjobs;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,11 +14,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -32,6 +39,9 @@ public class newPostFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private static final int REQUEST_IMAGE_CAPTURE = 111;
+    private static String[] REQUIRED_PERSMISSIONS = {Manifest.permission.CAMERA};
+    private static int REQUESTED_CODE_PERMISSIONS = 10;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -41,9 +51,14 @@ public class newPostFragment extends Fragment {
     ImageButton cameraButton;
     ImageButton galleryButton;
     ImageButton addPicButton;
+    ImageButton addPicButton2;
     EditText newPostTitle;
     String mTitleText;
     ImageView pic1;
+    ImageView pic2;
+    int picCount = 0;
+    FrameLayout image2Layout;
+
 
 
     public newPostFragment() {
@@ -89,18 +104,19 @@ public class newPostFragment extends Fragment {
 
         InitfindViews(v);
 
+
         newPostTitle.addTextChangedListener(newPostInputWatcher);
-        addPicButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cameraSession = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraSession, REQUEST_IMAGE_CAPTURE);
-            }
-        });
+        addPicButton.setOnClickListener(addPicAction);
 
 
         return v;
     }
+    View.OnClickListener addPicAction = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            cameraPermission();
+        }
+    };
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -141,12 +157,62 @@ public class newPostFragment extends Fragment {
         newPostTitle = (EditText) v.findViewById(R.id.newPost_titletext_EditText);
         pic1 = (ImageView) v.findViewById(R.id.newPost_image1_imageView);
         addPicButton = v.findViewById(R.id.newPost_imageButton1_imageButton);
+        pic2 = (ImageView) v.findViewById(R.id.newPost_image2_imageView);
+        addPicButton2 = v.findViewById(R.id.newPost_imageButton2_imageButton);
+        image2Layout = v.findViewById(R.id.newPost_secondImage_FrameLayout);
     }
 
-    private void displayPicture(Bitmap image){
-        addPicButton.setVisibility(View.GONE);
-        pic1.setVisibility(View.VISIBLE);
-        pic1.setImageBitmap(image);
+    private void displayPicture(Bitmap image) {
+        if (picCount == 0) {
+            addPicButton.setVisibility(View.GONE);
+            pic1.setVisibility(View.VISIBLE);
+            pic1.setImageBitmap(image);
+            image2Layout.setVisibility(View.VISIBLE);
+            addPicButton2.setOnClickListener(addPicAction);
+            picCount++;
+        } else {
+            addPicButton2.setVisibility(View.GONE);
+            pic2.setVisibility(View.VISIBLE);
+            pic2.setImageBitmap(image);
+
+        }
+
+    }
+
+    private boolean allPermissionGranted() {
+        int permissions_count = REQUIRED_PERSMISSIONS.length;
+        for (int i = 0; i < permissions_count; i++) {
+            if (ContextCompat.checkSelfPermission(getView().getContext(), REQUIRED_PERSMISSIONS[i]) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getView().getContext(), "Permissions failed.", Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUESTED_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
+                Intent cameraSession = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraSession, REQUEST_IMAGE_CAPTURE);
+            } else {
+                Toast.makeText(getView().getContext(), "Permission not granted by user.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    }
+    private void cameraPermission() {
+        if (allPermissionGranted()) {
+            Intent cameraSession = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraSession, REQUEST_IMAGE_CAPTURE);
+            Toast.makeText(getContext(), "Permissions Granted.", Toast.LENGTH_SHORT).show();
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), REQUIRED_PERSMISSIONS, REQUESTED_CODE_PERMISSIONS);
+            Toast.makeText(getContext(), "Not all permission granted", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
